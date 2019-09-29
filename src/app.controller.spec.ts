56b62a41a5f32463a4771a25c35e905246b793dd
vist wang <>
2019-09-29 15:27:27 +0800

    20190929

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -19,3 +20,27 @@ describe('AppController', () => {
     });
   });
 });
+
+// 装饰器名称	描述
+// @Request()	对应Express的req，也可以简写@req
+// @Response()	对应Express的res，也可以简写@res
+// @Next()	对应Express的next
+// @Session()	对应Express的req.session
+// @Param(param?: string)	对应Express的req.params
+// @Body(param?: string)	对应Express的req.body
+// @Query(param?: string)	对应Express的req.query
+// @Headers(param?: string)	对应Express的req.headers
+// 先说方法装饰器：
+
+// 装饰器名称	描述
+// @Post()	对应Express的Post方法
+// @Get()	对应Express的Get方法
+// @Put()	对应Express的Put方法
+// @Delete()	对应Express的Delete方法
+// @All()	对应Express的All方法
+// @Patch()	对应Express的Patch方法
+// @Options()	对应Express的Options方法
+// @Head()	对应Express的Head方法
+// @Render()	对应Express的res.render方法
+// @Header()	对应Express的res.header方法
+// @HttpCode()	对应Express的res.status方法，可以配合HttpStatus枚举
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,12 @@ import { ConfigModule } from './config/config.module';
 import { CoreModule } from './core/core.module';
 import { FeatureModule } from './feature/feature.module';
 import { SharedModule } from './shared/shared.module';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [ConfigModule, CoreModule, FeatureModule, SharedModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 // CoreModule 核心模块（注册中间件，过滤器，管道，守卫，拦截器，装饰器等）
 // SharedModule 共享模块（注册服务，mongodb，redis等）
