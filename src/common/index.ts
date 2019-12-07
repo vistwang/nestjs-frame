d62211170e5f2a56e4d50f572eab76b61e90ecf2
vist wang <vistwang@gmail.com>
2019-12-07 20:06:09 +0800

    update

---
+++ src/common/index.ts
@@ -0,0 +1,4 @@
+// worker_threads（工作线程）
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
+
+export const xxx = 1000;
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { BaseService } from './base/base.service';
 import { WebsockesModule } from './websockes/websockes.module';
 
 // 共享模块（共享mongodb、redis封装服务、通用服务）
 @Module({
-  providers: [BaseService],
+  providers: [],
   imports: [WebsockesModule],
 })
 export class SharedModule {}
